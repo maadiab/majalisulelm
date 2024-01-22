ce420300b235feab7e42170ffacb2fe388902cb4
root <root@DESKTOP-J0623AK>
2024-01-22 23:29:33 +0300

    add cores

---
+++ core/lessons.go
@@ -0,0 +1,12 @@
+package core
+
+type Lessons struct {
+	ID       int
+	Name     string
+	Type     string
+	Duration string
+	Author   string
+	Link     string
+	Time     string
+	Location string
+}
---
+++ core/user.go
@@ -0,0 +1,9 @@
+package core
+
+type User struct {
+	ID       int
+	Name     string
+	Mobile   int64
+	Email    string
+	Password string
+}
